455cf39b34fe37b3a82c5b8f3de90a60d18eb633
Mateusz <matyd356@gmail.com>
2020-04-18 14:42:00 +0200

    add undo functionality

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-let displayInput = document.querySelector(".display-input")
-let numberAndExecutionButtons = document.querySelectorAll(".operation-button, .number-button")
+let displayInput = document.querySelector(".display-input");
+let numberAndExecutionButtons = document.querySelectorAll(".operation-button, .number-button");
+let equalButton = document.querySelector(".equal");
+let undoButton = document.querySelector(".undo")
 
 //key handling
 window.addEventListener("keydown", (e) => {
     if (/[0-9]/gi.test(e.key)) {
         displayInput.textContent += e.key;
+    } else if (e.key == "Backspace") {
+        undo();
     }
 });
 
@@ -14,5 +18,14 @@ window.addEventListener("keydown", (e) => {
         displayInput.textContent += e.target.textContent;
     });
 });
+//undo function
+const undo = () => {
+    displayInput.textContent = displayInput.textContent.split("").slice(0, -1).join("")
+}
+undoButton.addEventListener("click", undo)
 
 //evaluating input
+//eval sub console.log((Function('"use strict"; return (' + displayInput.textContent + ')'))())
+equalButton.addEventListener("click", (() => {
+    console.log('xd')
+}));
