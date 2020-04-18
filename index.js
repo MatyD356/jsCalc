de84094ef5986e941a173355c47fcfe06c6e5813
Mateusz <matyd356@gmail.com>
2020-04-18 15:15:19 +0200

    improve key handling and clean up code

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-let displayInput = document.querySelector(".display-input");
-let numberAndExecutionButtons = document.querySelectorAll(".operation-button, .number-button");
-let equalButton = document.querySelector(".equal");
-let undoButton = document.querySelector(".undo")
-let clearButton = document.querySelector(".clear")
+/*----VARIABLES----*/
+const displayInput = document.querySelector(".display-input");
+const numberAndExecutionButtons = document.querySelectorAll(".operation-button, .number-button");
+const equalButton = document.querySelector(".equal");
+const undoButton = document.querySelector(".undo")
+const clearButton = document.querySelector(".clear")
 
+/*----FUNCTIONS----*/
 //clear function
 const clear = () => {
     displayInput.textContent = "";
-}
+};
+
 //clear function
 const undo = () => {
     displayInput.textContent = displayInput.textContent.split("").slice(0, -1).join("")
-}
+};
 
+//eval func
+//eval sub console.log((Function('"use strict"; return (' + displayInput.textContent + ')'))())
+const execute = () => {
+    console.log('xd')
+};
+
+/*----EVENT HANDLING----*/
 //key handling
 window.addEventListener("keydown", (e) => {
     if (/[0-9]/gi.test(e.key)) {
         displayInput.textContent += e.key;
-    } else if (e.key == "Backspace") {
+    } else if (/^(\+|-|\*|\/|\||\^|\(|.|\))$/gi.test(e.key)) {
+        displayInput.textContent += e.key;
+    } else if (/(=)/gi.test(e.key)) {
+        execute();
+    }
+    else if (e.key == "Backspace") {
         undo();
     }
 });
@@ -30,9 +45,4 @@ window.addEventListener("keydown", (e) => {
 });
 undoButton.addEventListener("click", undo)
 clearButton.addEventListener("click", clear)
-
-//evaluating input
-//eval sub console.log((Function('"use strict"; return (' + displayInput.textContent + ')'))())
-equalButton.addEventListener("click", (() => {
-    console.log('xd')
-}));
+equalButton.addEventListener("click", execute);
